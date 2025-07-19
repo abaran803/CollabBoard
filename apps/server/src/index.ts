b4bf358c21030cf3de3aa520c4748fdc3e3e860d
Ayush Baranwal <abaran803@gmail.com>
2025-07-19 21:59:55 +0530

    env and cors setup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,12 @@
+import '@/helpers/loadEnv';
+
+// app.ccaconst express = require("express");
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
-const PORT = 5000;
+app.use(cors()); // Enable CORS for cross-origin requests
+const PORT = process.env.PORT || 5000;
 
 app.get('/api/hello', (_, res) => {
   res.json({ message: 'Hello from server!' });
